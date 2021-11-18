2ea91d44847879c6d46c32e149ce18781eaf2431
Maddy <gillism5@msu.edu>
2021-11-18 12:47:16 -0500

    css addition

---
+++ soundboard.js
@@ -0,0 +1,15 @@
+window.onload = function() {
+    
+    var buttonElement = document.getElementById('my-first-button')
+
+    buttonElement.addEventListener('click', function () {
+        console.log('I was clicked!')
+      })
+
+    <button id="my-first-button">
+    I'm a button!
+    </button>
+
+
+
+}
